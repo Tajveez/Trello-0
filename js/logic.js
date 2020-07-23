43c38eea00fdc53671dde32b64a8d545a06949d8
Tajveez <tajveez786@gmail.com>
2020-07-23 23:05:46 +0500

    code changes

--- js/logic.js
+++ js/logic.js
@@ -1,1 +1,12 @@
-console.log("ello");
+let button = document.getElementById("add_new");
+let count = 0;
+let content = document.getElementById("content");
+content.innerHTML += "<br/>";
+button.addEventListener("click", () => {
+  if (count < 10) {
+    content.innerHTML +=
+      "<input type='text' value='test..." + count + "' /><br/>";
+    count++;
+    console.log(count);
+  }
+});
