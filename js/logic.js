ba52f1511569bc2f39cdc91fb5f805c336fa023b
Tajveez <tajveez786@gmail.com>
2020-07-26 21:54:39 +0500

    code changes

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,31 @@
-let button = document.getElementById("add_new");
-let count = 0;
-let content = document.getElementById("content");
-content.innerHTML += "<br/>";
-button.addEventListener("click", () => {
-  if (count < 10) {
-    content.innerHTML +=
-      "<input type='text' value='test..." + count + "' /><br/>";
-    count++;
-    console.log(count);
+let addPanel = document.getElementById("add_panel");
+let panelCount = 0;
+let panel = document.querySelector(".panel");
+panel.innerHTML += "<br/>";
+addPanel.addEventListener("click", () => {
+  if (panelCount < 5) {
+    panel.innerHTML +=
+      "<div class='panel-content' id='panel-content-"+panelCount+"'> panel# " +
+      panelCount +
+      "<button id='add_stack' class='add_stack' id='add_stack'>Add Stack +</button></div > <br />";
+    panelCount++;
+    console.log(panelCount);
+  }
+});
+let addStack = document.querySelector(".add_stack");
+
+// addStack.addEventListener("click", () => {});
+stackCount = 0;
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "add_stack") {
+    //do something
+    let panel_content = document.querySelector(".panel-content");
+
+    if (stackCount < 10) {
+      panel_content.innerHTML +=
+        "<input type='text' value='test..." + stackCount + "' /><br/>";
+      stackCount++;
+      console.log(stackCount);
+    }
   }
 });
