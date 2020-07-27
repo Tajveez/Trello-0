ce5d4f7d94b392f251894b8f70c59f1e08b1e41f
Tajveez <tajveez786@gmail.com>
2020-07-27 23:07:45 +0500

    code improvement

--- js/logic.js
+++ js/logic.js
@@ -7,7 +7,7 @@ addPanel.addEventListener("click", () => {
     panel.innerHTML +=
       "<div class='panel_content' id='panel_content-"+panelCount+"'> panel# " +
       panelCount +
-      "<button id='add_stack-"+panelCount+"' class='add_stack'>Add Stack +</button></div > <br />";
+      " <button id='add_stack-"+panelCount+"' class='add_stack'>Add Stack +</button></div > <br />";
     panelCount++;
     console.log(panelCount);
   }
@@ -15,17 +15,22 @@ addPanel.addEventListener("click", () => {
 let addStack = document.querySelector(".add_stack");
 
 // addStack.addEventListener("click", () => {});
-stackCount = 0;
 document.addEventListener("click", function (e) {
-  console.log(e);
+  // console.log(e);
   let target = e.target.id.split('-');
   if (e.target && target[0] == "add_stack") {
     //do something
     let panel_content = document.querySelector("#panel_content-"+target[1]);
+    // console.log(panel_content);
+    let secondLastEl = panel_content.lastElementChild.previousElementSibling;
+    let stackCount = 0;
+    if(secondLastEl !== null){
+      stackCount = parseInt(secondLastEl.id.split('-')[1]) + 1;
+    }
 
     if (stackCount < 10) {
       panel_content.innerHTML +=
-        "<input type='text' value='test..." + stackCount + "' /><br/>";
+        "<input type='text' id='stack-"+stackCount+"' value='test..." + stackCount + "' /><br/>";
       stackCount++;
       console.log(stackCount);
     }
